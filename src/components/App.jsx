cd17399622d488410ecaca8fb55b4b8effdf4e65
Yoshiki-Itagaki <b1700336@gl.aiu.ac.jp>
2024-02-13 10:28:19 +0900

    Created a counter program using useState hook in react.js

---
+++ src/components/App.jsx
@@ -0,0 +1,24 @@
+import React, {useState} from "react";
+
+function App() {
+
+    const [count, setCount] = useState(0)
+
+    async function increase(){
+      setCount(count+1)
+    }
+
+    async function decrease(){
+      setCount(count-1)
+    }
+    
+    return(
+      <div className="container">
+        <h1>{count}</h1>
+        <button onClick = {increase}>+</button>
+        <button onClick = {decrease}>-</button>
+      </div>
+    );
+}
+
+export default App;
